0d4a386e7ef053a5d6188df0c4ab469faea662eb
Steve Tractenberg <steve@blacks4.com>
2018-06-26 15:54:26 -0400

    blah

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+bash 'apt-get' do
+  code 'sudo apt-get update -y'
+  action :run
+end
+
+package 'apache2'
+
+service 'apache2' do
+  action [:enable, :start]
+end
